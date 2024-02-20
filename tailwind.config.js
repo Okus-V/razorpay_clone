c5e544d1a33907bb9f28fdab0435291ff1151ece
Vedant bhoir <vedantb247@gmail.com>
2024-02-21 02:52:54 +0530

    razorpay

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+
+/** @type {import('tailwindcss').Config} */
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'dark-blue': '#02042c',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'light-blue': '	#4299e1'
+  
+  
+}
+
+
+
+
+
+  },
+  plugins: [],
+}
